b833538037a95fb87d0d3ab824f5ba653decff9f
Wahid7852 <wahidzk0091@gmail.com>
2022-12-10 19:24:14 +0530

    Anykernel: rmx3461 bringup

--- anykernel.sh
+++ anykernel.sh
@@ -10,15 +10,14 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=begonia
-device.name2=begoniain
+device.name1=rmx3461
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
 no_block_display=true;
 
